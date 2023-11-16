3e674e68aa0c180482a9989ec1ab28d26d97a0e7
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2023-11-16 06:27:53 -0500

    Changed terminal operation from .collect(Collectors.toCollection(ArrayList::new)); to .collect(Collectors.toList()); It reads better
    all one liners if() were provided with {} blocks for better readability

--- src/array2/ArrayTwo.java
+++ src/array2/ArrayTwo.java
@@ -1,28 +1,18 @@
 package array2;
 
 import java.util.*;
-import java.util.function.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import javax.swing.plaf.basic.BasicTreeUI.TreeToggleAction;
 
 public class ArrayTwo {
 	public int countEvens(int[] nums) {
 		Integer count = 0;
 		for (int i : nums)
-			if (i % 2 == 0)
+			if (i % 2 == 0) {
 				count++;
+			}
 		return count;
 	}
 
@@ -34,16 +24,16 @@ public class ArrayTwo {
 
 	public int centeredAverage(int[] nums) {
 		Arrays.sort(nums);
-		List<Integer> nl = new ArrayList<>(
-				Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new)));
+		List<Integer> nl = new ArrayList<>(Arrays.stream(nums).boxed().collect(Collectors.toList()));
 		int sum = nl.stream().skip(1).limit(nl.size() - 2).reduce(0, (a, b) -> a + b);
 		return sum / (nl.size() - 2);
 	}
 
 	public int sum13(int[] nums) {
-		if (nums.length - 1 == -1)
+		if (nums.length - 1 == -1) {
 			return 0;
-		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		}
+		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		for (ListIterator<Integer> li = nl.listIterator(); li.hasNext();) {
 			if (li.next() == 13 && li.hasNext()) {
 				li.remove();
@@ -51,13 +41,14 @@ public class ArrayTwo {
 				li.remove();
 			}
 		}
-		if (!nl.isEmpty() && nl.get(nl.size() - 1) == 13)
+		if (!nl.isEmpty() && nl.get(nl.size() - 1) == 13) {
 			nl.remove(nl.size() - 1);
+		}
 		return nl.stream().reduce(0, (a, b) -> a + b);
 	}
 
 	public int sum67(int[] nums) {
-		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		for (ListIterator<Integer> li = nl.listIterator(); li.hasNext();) {
 			if (li.next() == 6) {
 				li.remove();
@@ -70,25 +61,28 @@ public class ArrayTwo {
 	}
 
 	public boolean has22(int[] nums) {
-		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
-		if (!nl.contains(2))
+		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toList());
+		if (!nl.contains(2)) {
 			return false;
+		}
 		for (ListIterator<Integer> li = nl.listIterator(); li.hasNext();)
-			if (li.next() == 2 && li.hasNext() && li.next() == 2)
+			if (li.next() == 2 && li.hasNext() && li.next() == 2) {
 				return true;
+			}
 		return false;
 	}
 
 	public boolean lucky13(int[] nums) {
-		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		return !nl.contains(1) && !nl.contains(3);
 	}
 
 	public boolean sum28(int[] nums) {
 		List<Integer> twosList = new ArrayList<>();
 		for (int i : nums)
-			if (i == 2)
+			if (i == 2) {
 				twosList.add(2);
+			}
 		return twosList.stream().reduce(0, (a, b) -> a + b) == 8;
 	}
 
@@ -97,8 +91,9 @@ public class ArrayTwo {
 		result.putIfAbsent(1, 0);
 		result.putIfAbsent(4, 0);
 		for (int i : nums)
-			if (result.containsKey(i))
+			if (result.containsKey(i)) {
 				result.computeIfPresent(i, (a, b) -> ++b);
+			}
 		return result.get(1) > result.get(4);
 	}
 
@@ -112,8 +107,9 @@ public class ArrayTwo {
 
 	public boolean only14(int[] nums) {
 		for (int i : nums)
-			if (i != 1 && i != 4)
+			if (i != 1 && i != 4) {
 				return false;
+			}
 		return true;
 	}
 
@@ -126,29 +122,32 @@ public class ArrayTwo {
 	}
 
 	public boolean no14(int[] nums) {
-		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		return (!nl.contains(1) || !nl.contains(4));
 	}
 
 	public boolean isEverywhere(int[] nums, int val) {
-		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		List<Integer> nl = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		int ultimateValInd = (nl.indexOf(val) % 2 == 0) ? 0 : 1;
-		if (nl.isEmpty() || nl.indexOf(val) == -1 && nl.size() == 1 || nl.get(nl.size() - 1) == 5) // STUPID WORK-AROUND
+		if (nl.isEmpty() || nl.indexOf(val) == -1 && nl.size() == 1 || nl.get(nl.size() - 1) == 5) { // STUPID WORK-AROUND
 			return true;
+		}
 		for (int i = 0; i < nl.size() - 1;) {
 			List<Integer> pair = nl.subList(i, i + 2);
 			if (pair.indexOf(val) == -1 || pair.indexOf(val) != ultimateValInd
-					|| pair.size() == 1 && pair.size() - 1 != ultimateValInd)
+					|| pair.size() == 1 && pair.size() - 1 != ultimateValInd) {
 				return false;
+			}
 			i += 2;
 		}
 		return (nl.size() % 2 != 0 && nl.get(nl.size() - 1) != val) ? false : true;
 	}
 
 	public boolean either24(int[] nums) {
-		List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
-		if (numsList.indexOf(2) == -1 && numsList.indexOf(4) == -1)
+		List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+		if (numsList.indexOf(2) == -1 && numsList.indexOf(4) == -1) {
 			return false;
+		}
 		boolean hasTwoNextTwo = false;
 		boolean hasFourNextFour = false;
 		int prior = 0;
@@ -172,7 +171,7 @@ public class ArrayTwo {
 	}
 
 	public boolean has77(int[] nums) {
-		List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		List<Integer> sevenIndexes = new ArrayList<>();
 		for (int i = 0; i < numsList.size(); i++) {
 			if (numsList.get(i) == 7) {
@@ -185,88 +184,98 @@ public class ArrayTwo {
 			for (ListIterator<Integer> iterator = sevenIndexes.listIterator(sevenIndexes.size()); iterator
 					.hasPrevious();) {
 				int differance = iterator.previous() - ((iterator.hasPrevious()) ? iterator.previous() : 0);
-				if (differance == 2 || differance == 1)
+				if (differance == 2 || differance == 1) {
 					return true;
+				}
 			}
 		}
 		return false;
 	}
+
 	public boolean has12(int[] nums) {
-		List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
-		return numsList.lastIndexOf(2) > numsList.indexOf(1) && 
-		(numsList.contains(1) && numsList.contains(2));
+		List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+		return numsList.lastIndexOf(2) > numsList.indexOf(1) && (numsList.contains(1) && numsList.contains(2));
 	}
+
 	public boolean modThree(int[] nums) {
-		if(nums.length < 3) return false;
+		if (nums.length < 3) {
+			return false;
+		}
 		boolean isPrevEven = (nums[0] % 2 == 0);
 		boolean isCurrentEven;
-		int oddStreak = (isPrevEven)? 0 : 1; 
-		int evenStreak = (isPrevEven)? 1 : 0; 
-		for(int i = 1; i < nums.length; i++) {
-			isCurrentEven = nums[i] % 2 == 0 ;
-			if( isCurrentEven && isPrevEven) {
+		int oddStreak = (isPrevEven) ? 0 : 1;
+		int evenStreak = (isPrevEven) ? 1 : 0;
+		for (int i = 1; i < nums.length; i++) {
+			isCurrentEven = nums[i] % 2 == 0;
+			if (isCurrentEven && isPrevEven) {
 				++evenStreak;
-			} else if( !isCurrentEven && !isPrevEven) {
+			} else if (!isCurrentEven && !isPrevEven) {
 				++oddStreak;
 			} else if (evenStreak == 3 || oddStreak == 3) {
 				return true;
 			} else {
 				isPrevEven = !isPrevEven;
-				evenStreak = (isCurrentEven)? 1 : 0;
-				oddStreak = (isCurrentEven)? 0 : 1;
+				evenStreak = (isCurrentEven) ? 1 : 0;
+				oddStreak = (isCurrentEven) ? 0 : 1;
 			}
 		}
 		return (evenStreak >= 3 || oddStreak >= 3);
 	}
-	
+
 	public boolean haveThree(int[] nums) {
 		int quantityOfThree = (nums[0] == 3) ? 1 : 0;
 		final int ALLOWED_QUANT_OF_THREE = 3;
 		int prevNumber;
-		for(int i = 1; i < nums.length; i++) {
+		for (int i = 1; i < nums.length; i++) {
 			prevNumber = nums[i - 1];
-			quantityOfThree = (nums[i] == 3)? quantityOfThree + 1 : quantityOfThree;
-			if(nums[i] == 3 && prevNumber == 3 || quantityOfThree > ALLOWED_QUANT_OF_THREE)
+			quantityOfThree = (nums[i] == 3) ? quantityOfThree + 1 : quantityOfThree;
+			if (nums[i] == 3 && prevNumber == 3 || quantityOfThree > ALLOWED_QUANT_OF_THREE) {
 				return false;
+
+			}
 		}
 		return quantityOfThree == 3;
-	}	
-	
+	}
+
 	public boolean twoTwo(int[] nums) {
 		List<Integer> indexesOfTwo = new ArrayList<>();
-		for(int index = 0; index < nums.length; index ++ ){
-			if(nums[index] == 2)
+		for (int index = 0; index < nums.length; index++) {
+			if (nums[index] == 2) {
 				indexesOfTwo.add(index + 1);
-		} 
-		if(indexesOfTwo.isEmpty()) {
+			}
+		}
+		if (indexesOfTwo.isEmpty()) {
 			return true;
-		} else if(indexesOfTwo.size() == 1)  {
+		} else if (indexesOfTwo.size() == 1) {
 			return false;
 		}
 		BiPredicate<Integer, Integer> areNumbersNextToEachOther = (a, b) -> b - a == 1;
-		for(int j = 0; j < indexesOfTwo.size() - 1 ; j++ ) {
-			if(!areNumbersNextToEachOther.test(indexesOfTwo.get(j), indexesOfTwo.get(j + 1))) 
+		for (int j = 0; j < indexesOfTwo.size() - 1; j++) {
+			if (!areNumbersNextToEachOther.test(indexesOfTwo.get(j), indexesOfTwo.get(j + 1))) {
 				return false;
+			}
 		}
 		return true;
 	}
+
 	public boolean sameEnds(int[] nums, int len) {
-		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		List<Integer> beginningOfNums = numsCollection.subList(0, len);
 		List<Integer> endingOfNums = numsCollection.subList(numsCollection.size() - len, numsCollection.size());
 		return beginningOfNums.equals(endingOfNums);
 	}
 
 	public boolean tripleUp(int[] nums) {
-		if(nums.length < 3)
+		if (nums.length < 3) {
 			return false;
+		}
 		final int MIN_ADJACENT_STREAK = 2;
 		int adjacentCount = 0;
-		for(int i = 0; i < nums.length; i++) {
+		for (int i = 0; i < nums.length; i++) {
 			int nextIndex = i + 1;
-			if( nextIndex <= nums.length-1 && nums[nextIndex] - nums[i] == 1)  {
+			if (nextIndex <= nums.length - 1 && nums[nextIndex] - nums[i] == 1) {
 				++adjacentCount;
-			} else if(adjacentCount >= MIN_ADJACENT_STREAK) {
+			} else if (adjacentCount >= MIN_ADJACENT_STREAK) {
 				return true;
 			} else {
 				adjacentCount = 0;
@@ -274,95 +283,96 @@ public class ArrayTwo {
 		}
 		return adjacentCount >= MIN_ADJACENT_STREAK;
 	}
-	
+
 	public int[] fizzArray3(int start, int end) {
 		List<Integer> result = new ArrayList<>();
-		IntStream.range(start, end)
-			.forEach(num -> {
-				result.add(num);
-			});
-		return result.stream().mapToInt(Integer::intValue).toArray();	
+		IntStream.range(start, end).forEach(num -> {
+			result.add(num);
+		});
+		return result.stream().mapToInt(Integer::intValue).toArray();
 	}
 
 	public int[] shiftLeft(int[] nums) {
-		if(nums.length <= 1)
+		if (nums.length <= 1) {
 			return nums;
+		}
 		Deque<Integer> result = new ArrayDeque<>();
 		result.addLast(nums[0]);
-		for(int j = nums.length-1; j >= 1; j--) {
+		for (int j = nums.length - 1; j >= 1; j--) {
 			result.addFirst(nums[j]);
 		}
 		return result.stream().mapToInt(Integer::intValue).toArray();
 	}
-	
+
 	public int[] tenRun(int[] nums) {
-		boolean hasMultOfTen = Arrays.stream(nums)
-			.anyMatch(n -> n % 10 == 0);
-		if( !hasMultOfTen)
+		boolean hasMultOfTen = Arrays.stream(nums).anyMatch(n -> n % 10 == 0);
+		if (!hasMultOfTen) {
 			return nums;
-		
-		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		}
+
+		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		int currentMultOfTen = -1;
 		int priorMultOfTen = -1;
-		
-		for(int i = 0; i < numsCollection.size(); i++) {
+
+		for (int i = 0; i < numsCollection.size(); i++) {
 			boolean isMultipleOfTen = numsCollection.get(i) % 10 == 0;
-			if(isMultipleOfTen && priorMultOfTen != numsCollection.get(i)) {
+			if (isMultipleOfTen && priorMultOfTen != numsCollection.get(i)) {
 				currentMultOfTen = numsCollection.get(i);
-			} 
-			if(currentMultOfTen != priorMultOfTen) {
+			}
+			if (currentMultOfTen != priorMultOfTen) {
 				numsCollection.set(i, currentMultOfTen);
 			}
-			if(isMultipleOfTen && numsCollection.get(i) != currentMultOfTen) {
+			if (isMultipleOfTen && numsCollection.get(i) != currentMultOfTen) {
 				priorMultOfTen = currentMultOfTen;
 				currentMultOfTen = numsCollection.get(i);
 			}
 		}
 		return numsCollection.stream().mapToInt(Integer::intValue).toArray();
 	}
+
 	public int[] pre4(int[] nums) {
-		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		int firstFourIndex = numsCollection.indexOf(4);
-		if(firstFourIndex == -1) 
+		if (firstFourIndex == -1) {
 			return nums;
-		return numsCollection.subList(0, firstFourIndex).stream()
-			.mapToInt(Integer::intValue)
-			.toArray();
+		}
+		return numsCollection.subList(0, firstFourIndex).stream().mapToInt(Integer::intValue).toArray();
 	}
 
 	public int[] post4(int[] nums) {
-		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toList());
 		int lastFourIndex = numsCollection.lastIndexOf(4);
-		return numsCollection.subList(lastFourIndex + 1, numsCollection.size()).stream()
-			.mapToInt(Integer::intValue)
-			.toArray();
+		return numsCollection.subList(lastFourIndex + 1, numsCollection.size()).stream().mapToInt(Integer::intValue)
+				.toArray();
 	}
 
 	public int[] notAlone(int[] nums, int val) {
-		if(nums.length < 3)
+		if (nums.length < 3) {
 			return nums;
-		
+		}
+
 		int[] arrCopy = Arrays.copyOf(nums, nums.length);
 		int current = 0;
 		int next = 0;
 		int previous = 0;
-		
-		for(int j = 1; j < arrCopy.length - 1; j++) {
+
+		for (int j = 1; j < arrCopy.length - 1; j++) {
 			current = arrCopy[j];
 			next = arrCopy[j + 1];
 			previous = arrCopy[j - 1];
-			if(current == val && previous != val && next != val ) {
-				int greaterIndex = (Math.max(previous, next) == previous)?  j - 1 : j + 1;  
+			if (current == val && previous != val && next != val) {
+				int greaterIndex = (Math.max(previous, next) == previous) ? j - 1 : j + 1;
 				arrCopy[greaterIndex] = Math.max(previous, next);
 				arrCopy[j] = Math.max(previous, next);
 			}
 		}
 		return arrCopy;
 	}
+
 	public int[] zeroFront(int[] nums) {
 		Deque<Integer> result = new ArrayDeque<>();
-		for(int n : nums) {
-			if(n == 0){
+		for (int n : nums) {
+			if (n == 0) {
 				result.addFirst(n);
 			} else {
 				result.addLast(n);
@@ -374,8 +384,8 @@ public class ArrayTwo {
 	public int[] withoutTen(int[] nums) {
 		List<Integer> zeros = new ArrayList<>();
 		List<Integer> result = new ArrayList<>();
-		for(int n : nums) {
-			if(n == 10){
+		for (int n : nums) {
+			if (n == 10) {
 				zeros.add(0);
 			} else {
 				result.add(n);
@@ -384,61 +394,61 @@ public class ArrayTwo {
 		result.addAll(zeros);
 		return result.stream().mapToInt(Integer::intValue).toArray();
 	}
-	
+
 	public int[] zeroMax(int[] nums) {
 		Function<int[], Integer> getGreatestOddNumber = (arrOfNums) -> {
 			int greatestOddFound = -1;
-			for(int i = 0; i < arrOfNums.length; i++) {
-				if(arrOfNums[i] % 2 != 0 && arrOfNums[i] > greatestOddFound) {
+			for (int i = 0; i < arrOfNums.length; i++) {
+				if (arrOfNums[i] % 2 != 0 && arrOfNums[i] > greatestOddFound) {
 					greatestOddFound = arrOfNums[i];
 				}
 			}
 			return greatestOddFound;
 		};
-		for(int i = 0; i < nums.length; i++) {
-			if(nums[i] == 0){
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
 				int greatestOddFound = getGreatestOddNumber.apply(Arrays.copyOfRange(nums, i, nums.length));
-				nums[i] = (greatestOddFound != -1)? greatestOddFound : 0;
+				nums[i] = (greatestOddFound != -1) ? greatestOddFound : 0;
 			}
 		}
 		return nums;
 	}
+
 	public int[] evenOdd(int[] nums) {
-		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toCollection(ArrayList::new));
+		List<Integer> numsCollection = Arrays.stream(nums).boxed().collect(Collectors.toList());
 
 		Collections.sort(numsCollection, (Integer a, Integer b) -> {
-			if(a % 2 == 0) {
+			if (a % 2 == 0) {
 				return -1;
-			} else if( (a % 2 == 0 && b % 2 == 0)) {
+			} else if ((a % 2 == 0 && b % 2 == 0)) {
 				return 0;
-			} else return 1;
+			} else
+				return 1;
 		});
 		return numsCollection.stream().mapToInt(Integer::intValue).toArray();
 	}
+
 	public String[] fizzBuzz(int start, int end) {
 		List<String> result = new ArrayList<>();
 		Function<Integer, String> fizzBuzznezzChecker = num -> {
-			if(num % 3 == 0 && num % 5 == 0) {
+			if (num % 3 == 0 && num % 5 == 0) {
 				return "FizzBuzz";
 			} else if (num % 3 == 0) {
 				return "Fizz";
-			} else if (num % 5  == 0) {
+			} else if (num % 5 == 0) {
 				return "Buzz";
 			} else {
 				return String.valueOf(num);
 			}
 		};
-		for( int i = start; i < end ; i++){
+		for (int i = start; i < end; i++) {
 			String currentIndexTransformed = fizzBuzznezzChecker.apply(i);
 			result.add(currentIndexTransformed);
 		}
 		return result.toArray(new String[0]);
 	}
 
-
-
 	public static void main(String[] args) {
-		int[] num = {3, 3, 2};
 
 		ArrayTwo at = new ArrayTwo();
 		System.out.println(at.fizzBuzz(10, 40));
